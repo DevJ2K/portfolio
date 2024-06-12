4f5cf4393d4256a01f6ff48d115cd8a5299e2229
DevJ2K <ajavontheopro@gmail.com>
2024-06-12 16:14:44 +0200

    adding homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,13 @@
 <template>
-  <div>
+  <main class="container flex w-full">
+    <PresentationView/>
+  </main>
+  <!-- <div>
     <h1 class=" bg-blue-400">Portfolio</h1>
-  </div>
+  </div> -->
 </template>
 
 <script setup>
-
+import PresentationView from './home/PresentationView.vue'
 </script>
 
---
+++ src/views/home/PresentationView.vue
@@ -0,0 +1,16 @@
+<template>
+	<main class="flex h-screen w-full bg-blue-200">
+		<h1>Hello, I'm Theo Ajavon</h1>
+		<h2>Software Engineer & Swift Developer</h2>
+		<p>Voluptate irure consectetur exercitation esse labore occaecat Lorem laborum occaecat nisi dolore sint ad ea. Quis voluptate sunt in ipsum eu est aute sunt sunt minim incididunt reprehenderit. Ipsum aliquip id nostrud duis cupidatat.</p>
+		<button>Open Resume</button>
+
+		<div>
+			<a>O</a>
+		</div>
+	</main>
+</template>
+
+<script setup>
+
+</script>
