5b3ffdcdf0f3127f4f94a9cf12c5dba1383820e0
DevJ2K <ajavontheopro@gmail.com>
2024-06-14 18:05:54 +0200

    PENDING : Navigation bar

--- src/components/BackgroundSpotlight.vue
+++ src/components/BackgroundSpotlight.vue
@@ -5,7 +5,7 @@
 
 		<!-- <div class=" absolute left-0 top-0 h-[450vh] w-36 origin-top-left rotate-[-30deg] bg-accent-color/60 dark:bg-accent-color-dark/60 md:rotate-[-35deg]  lg:rotate-[-50deg]"></div> -->
 
-		<div class="absolute left-0 top-0 h-screen w-full bg-gradient-to-bl from-black from-40% via-accent-color via-50% to-black to-60%">
+		<div :class="`absolute left-0 top-0 h-screen w-full ${rotate} from-black from-40% via-accent-color via-50% to-black to-60%`">
 		</div>
 
 		<div class=" absolute left-0 top-0 h-screen w-full backdrop-blur-2xl"></div>
@@ -17,6 +17,10 @@ defineProps({
 	color: {
 		type: String,
 		default: 'bg-blue-500'
+	},
+	rotate: {
+		type: String,
+		default: "bg-gradient-to-bl"
 	}
 })
 </script>
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<!-- Image -->
+		<div class=" rounded-full"></div>
+		<h3>DevJ2K</h3>
+	</div>
+	<ul>
+		<li>AAA</li>
+		<li>AAA</li>
+		<li>AAA</li>
+		<li>AAA</li>
+		<li>ICON MOON</li>
+	</ul>
+</template>
+
+<script setup>
+
+</script>
