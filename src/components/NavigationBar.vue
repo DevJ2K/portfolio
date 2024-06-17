3266ae5a3cb9b0855fa8de08e3c09a267d1b099f
DevJ2K <ajavontheopro@gmail.com>
2024-06-17 12:19:36 +0200

    🚧 : Navigation bar...

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,21 +1,20 @@
 <template>
-	<div class="container fixed top-0 z-50 flex h-24 w-full items-center justify-between bg-black/40 backdrop-blur-md">
-		<div class="flex flex-row items-center gap-4">
-			<!-- Image -->
-
-
-			<div class=" size-16 overflow-hidden rounded-full border-2 bg-blue-400">
-				<img src="../assets/images/j2klogo.png" alt="">
+	<div class="fixed top-0 z-50 flex h-24 w-full bg-black/40 backdrop-blur-md">
+		<div class="container flex items-center justify-between">
+			<div class="flex flex-row items-center gap-4">
+				<div class=" size-16 overflow-hidden rounded-full border-2 bg-blue-400">
+					<img src="../assets/images/j2klogo.png" alt="" class=" size-full object-cover">
+				</div>
+				<h3 class=" font-bold text-primary">DevJ2K</h3>
 			</div>
-			<h3 class=" font-bold text-primary">DevJ2K</h3>
+			<ul class="flex flex-row gap-8 text-lg font-semibold text-primary">
+				<li>About</li>
+				<li>Skills</li>
+				<li>Projects</li>
+				<li>Contact</li>
+				<li><i class="fa-regular fa-moon"></i></li>
+			</ul>
 		</div>
-		<ul class="flex flex-row gap-8 text-lg font-semibold text-primary">
-			<li>About</li>
-			<li>Skills</li>
-			<li>Projects</li>
-			<li>Contact</li>
-			<li><i class="fa-regular fa-moon"></i></li>
-		</ul>
 	</div>
 </template>
 
