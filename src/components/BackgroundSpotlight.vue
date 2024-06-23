31ca9948c1f0be4168dd899a2ae4fe49a078df8f
DevJ2K <ajavontheopro@gmail.com>
2024-06-23 21:19:27 +0200

    ☁ : Saving status...

--- src/components/BackgroundSpotlight.vue
+++ src/components/BackgroundSpotlight.vue
@@ -5,13 +5,10 @@
   >
     <div :class="`absolute top-0 left-0 w-full h-[100%] bg-bg-${bgNb} dark:bg-d-bg-${bgNb} `"></div>
 
-    <!-- <div :class="`absolute top-0 left-0 w-full h-[100%] ${bgClass}`"></div> -->
 
-    <!-- <div class="absolute top-0 left-0 w-full h-[100%] bg-bg-1 dark:bg-d-bg-2"></div> -->
+    <!-- <div :class="`absolute left-0 top-0 h-[${lineSize}] w-full ${rotate} from-bg-${bgNb} from-40% via-accent-color via-50% to-bg-${bgNb} to-60% dark:from-d-bg-${bgNb} dark:from-40% dark:via-d-accent-color dark:via-50% dark:to-d-bg-${bgNb} dark:to-60%`"></div> -->
 
-    <!-- <div class=" absolute left-0 top-0 h-[450vh] w-36 origin-top-left rotate-[-30deg] bg-accent-color/60 dark:bg-accent-color-dark/60 md:rotate-[-35deg]  lg:rotate-[-50deg]"></div> -->
-
-    <!-- <div :class="`absolute left-0 top-0 h-[${lineSize}] w-full ${rotate} from-bg from-40% via-accent-color via-50% to-bg to-60% dark:from-d-bg dark:from-40% dark:via-d-accent-color dark:via-50% dark:to-d-bg dark:to-60%`"></div> -->
+    <!-- <div :class="`absolute left-0 top-0 h-[${lineSize}] w-full ${rotate} from-bg-${bgNb} from-40% via-accent-color via-50% to-bg-${bgNb} to-60% dark:from-d-bg-${bgNb} dark:from-40% dark:via-d-accent-color dark:via-50% dark:to-d-bg-${bgNb} dark:to-60%`"></div> -->
     <!-- <div class="absolute left-0 top-0 h-[60%] w-full -rotate-45 bg-gradient-to-r from-bg from-40% via-accent-color via-50% to-bg to-60% dark:from-d-bg dark:from-40% dark:via-d-accent-color dark:via-50% dark:to-d-bg dark:to-60%"></div> -->
 
     <!-- <div class="relative flex size-full items-center justify-center">
