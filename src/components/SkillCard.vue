602dd115b48fd4bdda23fedcffd7377d12cc7896
DevJ2K <ajavontheopro@gmail.com>
2024-06-26 16:03:28 +0200

    🚧 : Adding tooltip in Technical Skills...

--- src/components/SkillCard.vue
+++ src/components/SkillCard.vue
@@ -1,27 +1,46 @@
 <template>
-	<div class="group flex  flex-col items-center justify-center gap-4 rounded-lg border bg-black/15 p-4 shadow-md">
-		<div class="relative flex flex-row items-center justify-center gap-2">
-			<i :class="icon" class="text-lg font-bold max-sm:text-lg"></i>
-			<h1 class="text-sm font-bold">{{ skill }}</h1>
-			<!-- <p class="absolute top-0 z-50 w-32 rounded-md border bg-black p-2 text-xs opacity-0 transition-all group-hover:opacity-100">{{ description }}</p> -->
+	<!-- <div> -->
+		<div class="group flex  cursor-pointer flex-col items-center justify-center gap-4 rounded-lg border bg-black/15 p-4 shadow-md" @mouseover="displayParagraph" @mouseout="hideParagraph">
+			<div class="relative flex flex-row items-center justify-center gap-2">
+				<i :class="icon" class="text-lg font-bold max-sm:text-lg"></i>
+				<h1 class="text-sm font-bold">{{ skill }}</h1>
+				<p :id="skill" class="absolute top-0 z-50 rounded-md border bg-black p-2 text-xs transition-all" style="opacity: 0; visibility: hidden;">{{ description }}</p>
+			</div>
 		</div>
-	</div>
+	<!-- </div> -->
 </template>
 
 <script setup>
-defineProps({
+const props = defineProps({
 	skill: {
 		type: String,
 		default: "Skills"
 	},
 	description: {
 		type: String,
-		default: "Ea reprehenderit nisi nostrud in officia commodo Lorem aliquip cillum nisi do eiusmod consectetur Lorem. Culpa minim in reprehenderit magna quis nisi dolor. Nisi velit ex cillum eu. Sit pariatur officia laborum eiusmod consequat pariatur dolor nisi aute ut qui nostrud commodo dolore."
+		// default: "Ea reprehenderit nisi nostrud in officia commodo Lorem aliquip cillum nisi do eiusmod consectetur Lorem. Culpa minim in reprehenderit magna quis nisi dolor. Nisi velit ex cillum eu."
+		default: "ahetihbieihe. bitibtehibhthb. bieiebiebiheibebhi. betjewrghiwrhwrhgio. wrgihiwrghirwg",
 	},
 	icon: {
 		type: String,
 		default: "fa-solid fa-gear"
 	}
-})
+});
+
+const displayParagraph = () => {
+	// console.log("Display paragraph !");
+	document.getElementById(props.skill).style.visibility = 'visible';
+	document.getElementById(props.skill).style.opacity = 1;
+}
+
+const hideParagraph = () => {
+	// console.log("Hide paragraph !");
+	document.getElementById(props.skill).style.opacity = 0;
+	document.getElementById(props.skill).style.visibility = 'hidden';
+}
+
+// const myDiv = document.getElementById("myDiv");
+// myDiv.addEventListener("mouseover", displayParagraph);
+// myDiv.addEventListener("mouseleave", hideParagraph);
 </script>
 
--- src/views/home/SkillView.vue
+++ src/views/home/SkillView.vue
@@ -5,145 +5,63 @@
 		<main class=" container z-10 flex min-h-screen flex-col items-center  gap-9 text-primary dark:text-d-primary">
 			<h1 class="text-3xl underline decoration-accent-color underline-offset-8">Skills</h1>
 
+			<div class="flex flex-col gap-8">
+				<fieldset class="flex w-full items-center justify-center rounded-lg border bg-accent-color/15 px-6 py-4">
+					<legend class=" m-auto px-1 text-lg font-semibold">Technical Skills</legend> <!-- rounded-md border-->
 
-			<fieldset class="flex w-full items-center justify-center rounded-lg border bg-accent-color/15 p-3">
-				<legend class=" m-auto px-1 text-lg font-semibold">Technical Skills</legend> <!-- rounded-md border-->
+					<div class=" flex w-full flex-col items-center justify-center gap-4">
+							<div class=" grid w-full grid-cols-3 justify-center gap-3 max-sm:flex max-sm:flex-wrap">
+								<SkillCard skill="API" icon="fa-solid fa-cube" />
+								<SkillCard skill="Unit Test" icon="fa-solid fa-vials" />
+								<SkillCard skill="Peer To Peer Learning" icon="fa-solid fa-users" />
+							</div>
 
-				<div class=" flex w-full flex-col items-center justify-center gap-4">
-					<div class="container mb-4 flex flex-col items-center justify-center gap-3">
-						<div class=" grid w-full grid-cols-3 justify-center gap-3 max-sm:flex max-sm:flex-wrap">
-							<SkillCard skill="API" icon="fa-solid fa-cube" />
+							<div class=" grid w-full grid-cols-2 justify-center gap-3 max-sm:flex max-sm:flex-wrap">
+								<SkillCard skill="Oriented Object Programming" icon="fa-solid fa-diagram-project" />
+								<!-- Search by Apple -->
+								<SkillCard skill="Data structures and algorithms" icon="fa-solid fa-database" />
+								<!-- Search by Apple -->
+							</div>
 
-							<SkillCard skill="Unit Test" icon="fa-solid fa-vials" />
-
-							<SkillCard skill="Peer To Peer Learning" icon="fa-solid fa-users" />
-						</div>
-
-						<div class=" grid w-full grid-cols-2 justify-center gap-3 max-sm:flex max-sm:flex-wrap">
-							<SkillCard skill="Oriented Object Programming" icon="fa-solid fa-diagram-project" />
-							<!-- Search by Apple -->
-
-							<SkillCard skill="Data structures and algorithms" icon="fa-solid fa-database" />
-							<!-- Search by Apple -->
-						</div>
-
-						<div class=" grid w-full grid-cols-3 justify-center gap-3 max-sm:flex max-sm:flex-wrap">
-							<SkillCard skill="Debugging" icon="fa-solid fa-bug" /> <!-- Search by Apple -->
-
-							<SkillCard skill="Clean Code" icon="fa-solid fa-spray-can-sparkles" />
-
-							<SkillCard skill="Problem solving" icon="fa-solid fa-square-check" />
-							<!-- Search by Apple -->
-						</div>
+							<div class=" grid w-full grid-cols-3 justify-center gap-3 max-sm:flex max-sm:flex-wrap">
+								<SkillCard skill="Debugging" icon="fa-solid fa-bug" /> <!-- Search by Apple -->
+								<SkillCard skill="Clean Code" icon="fa-solid fa-spray-can-sparkles" />
+								<SkillCard skill="Problem solving" icon="fa-solid fa-square-check" />
+								<!-- Search by Apple -->
+							</div>
 					</div>
-				</div>
-			</fieldset>
+				</fieldset>
 
+				<fieldset class="flex w-full items-center justify-center rounded-lg border bg-accent-color/15 px-6 py-4">
+					<legend class=" m-auto px-1 text-lg font-semibold">Programming Languages</legend>
+					<div class="flex w-full flex-wrap justify-center gap-6">
+						<LogoSkill :logo="SwiftSvg" tech-name="Swift" />
+						<LogoSkill :logo="PythonSvg" tech-name="Python" />
+						<LogoSkill :logo="CSvg" tech-name="C" />
+						<LogoSkill :logo="CplusplusSvg" tech-name="C++" />
+						<LogoSkill :logo="JavascriptSvg" tech-name="Javascript" />
+					</div>
+				</fieldset>
 
-			<fieldset class="flex w-full items-center justify-center rounded-lg border bg-accent-color/15 p-3">
-				<legend class=" m-auto px-1 text-lg font-semibold">Programming Languages</legend>
-				<div class="flex w-full flex-wrap justify-around gap-4 md:gap-10">
-							<LogoSkill :logo="SwiftSvg" tech-name="Swift" />
-							<LogoSkill :logo="PythonSvg" tech-name="Python" />
-							<LogoSkill :logo="CSvg" tech-name="C" />
-							<LogoSkill :logo="CplusplusSvg" tech-name="C++" />
-							<LogoSkill :logo="JavascriptSvg" tech-name="Javascript" />
-				</div>
-			</fieldset>
-
-			<fieldset class="flex w-full items-center justify-center rounded-lg border bg-accent-color/15 p-3">
-				<legend class=" m-auto px-1 text-lg font-semibold">Frameworks & Librairies</legend>
-				<div class="flex w-full flex-wrap justify-around gap-4 md:gap-10">
+				<fieldset class="flex w-full items-center justify-center rounded-lg border bg-accent-color/15 px-6 py-4">
+					<legend class=" m-auto px-1 text-lg font-semibold">Frameworks & Librairies</legend>
+					<div class="flex w-full flex-wrap justify-center gap-6">
 						<LogoSkill :logo="FlutterSvg" tech-name="Flutter" />
 						<LogoSkill :logo="VueSvg" tech-name="Vue" />
 						<LogoSkill :logo="TailwindSvg" tech-name="Tailwind" />
 						<LogoSkill :logo="SeleniumSvg" tech-name="Selenium" />
-				</div>
-			</fieldset>
-
-			<fieldset class="flex w-full items-center justify-center rounded-lg border bg-accent-color/15 p-3">
-				<legend class=" m-auto px-1 text-lg font-semibold">Databases</legend>
-				<div class="flex w-full flex-wrap justify-around gap-4 md:gap-10">
-					<LogoSkill :logo="FirebaseSvg" tech-name="Firebase" />
-					<LogoSkill :logo="SqliteSvg" tech-name="Sqlite" />
-				</div>
-			</fieldset>
-
-
-
-
-			<!-- <div class="flex w-full flex-col items-center justify-center">
-
-				<div class=" flex w-full flex-col items-center justify-center gap-4">
-					<h3 class="text-center text-lg font-medium sm:text-xl">Technical Skills</h3>
-					<div class="container mb-4 flex flex-col items-center justify-center gap-3">
-						<div class=" grid w-full grid-cols-3 justify-center gap-3 max-sm:flex max-sm:flex-wrap">
-							<SkillCard skill="API" icon="fa-solid fa-cube" />
-
-							<SkillCard skill="Unit Test" icon="fa-solid fa-vials" />
-
-							<SkillCard skill="Peer To Peer Learning" icon="fa-solid fa-users" />
-						</div>
-
-						<div class=" grid w-full grid-cols-2 justify-center gap-3 max-sm:flex max-sm:flex-wrap">
-							<SkillCard skill="Oriented Object Programming" icon="fa-solid fa-diagram-project" />
-
-
-							<SkillCard skill="Data structures and algorithms" icon="fa-solid fa-database" />
-
-						</div>
-
-						<div class=" grid w-full grid-cols-3 justify-center gap-3 max-sm:flex max-sm:flex-wrap">
-							<SkillCard skill="Debugging" icon="fa-solid fa-bug" />
-
-							<SkillCard skill="Clean Code" icon="fa-solid fa-spray-can-sparkles" />
-
-							<SkillCard skill="Problem solving" icon="fa-solid fa-square-check" />
-
-						</div>
-
 					</div>
-					<div class="w-10/12 border opacity-25"></div>
-				</div>
+				</fieldset>
 
-
-				<div class="container z-10 flex w-full flex-col items-center  gap-9 text-primary dark:text-d-primary">
-					<div class="flex w-full flex-col items-center justify-center">
-						<h3 class=" mb-4 text-center text-sm font-medium sm:text-xl">Programming Languages</h3>
-						<div class="flex w-full flex-wrap justify-around gap-4 md:gap-10">
-							<LogoSkill :logo="SwiftSvg" tech-name="Swift" />
-							<LogoSkill :logo="PythonSvg" tech-name="Python" />
-							<LogoSkill :logo="CSvg" tech-name="C" />
-							<LogoSkill :logo="CplusplusSvg" tech-name="C++" />
-							<LogoSkill :logo="JavascriptSvg" tech-name="Javascript" />
-						</div>
+				<fieldset class="flex w-full items-center justify-center rounded-lg border bg-accent-color/15 p-3">
+					<legend class=" m-auto px-1 text-lg font-semibold">Databases</legend>
+					<div class="flex w-full flex-wrap justify-center gap-6">
+						<LogoSkill :logo="FirebaseSvg" tech-name="Firebase" />
+						<LogoSkill :logo="SqliteSvg" tech-name="Sqlite" />
 					</div>
+				</fieldset>
+			</div>
 
-					<div class="w-2/3 border opacity-25"></div>
-
-					<div class="flex w-full flex-col items-center justify-center">
-						<h3 class=" mb-4 text-sm font-medium sm:text-xl">Frameworks and Librairies</h3>
-						<div class="flex w-full flex-wrap justify-around gap-4 md:gap-10">
-							<LogoSkill :logo="FlutterSvg" tech-name="Flutter" />
-							<LogoSkill :logo="VueSvg" tech-name="Vue" />
-							<LogoSkill :logo="TailwindSvg" tech-name="Tailwind" />
-							<LogoSkill :logo="SeleniumSvg" tech-name="Selenium" />
-						</div>
-					</div>
-
-					<div class="w-1/3 border opacity-25"></div>
-
-					<div class="flex w-full flex-col items-center justify-center">
-						<h3 class=" mb-4 text-sm font-medium sm:text-xl">Databases</h3>
-						<div class="flex w-full flex-wrap justify-around gap-4 md:gap-10">
-							<LogoSkill :logo="FirebaseSvg" tech-name="Firebase" />
-							<LogoSkill :logo="SqliteSvg" tech-name="Sqlite" />
-						</div>
-					</div>
-
-					<div class="w-[6%] border opacity-25"></div>
-				</div>
-			</div> -->
 		</main>
 	</div>
 </template>
@@ -151,6 +69,7 @@
 <script setup>
 import BackgroundSpotlight from '@/components/BackgroundSpotlight.vue';
 import LogoSkill from '@/components/LogoSkill.vue';
+import SkillCard from '@/components/SkillCard.vue';
 
 // Importing Svg \\
 import SwiftSvg from '@/components/logos/skills/SwiftSvg.vue';
@@ -167,5 +86,4 @@ import SeleniumSvg from '@/components/logos/skills/SeleniumSvg.vue';
 import FirebaseSvg from '@/components/logos/skills/FirebaseSvg.vue';
 import SqliteSvg from '@/components/logos/skills/SqliteSvg.vue';
 
-import SkillCard from '@/components/SkillCard.vue';
 </script>
