0db77f431f44249cf66b6c77f4ce1897fcbaa8f2
DevJ2K <ajavontheopro@gmail.com>
2024-06-28 11:52:00 +0200

    🚧 : Adding colors on ProjectCard...

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,16 +1,16 @@
 <template>
-	<div class="flex max-w-xs flex-col items-start justify-start gap-4 rounded-2xl bg-accent-color/35 p-6 shadow-md">
+	<div class="flex max-w-xs flex-col items-start justify-start gap-4 rounded-2xl border border-ui-border bg-ui-bg p-6 shadow-md transition-all hover:border-hover-ui-border hover:shadow-lg dark:border-d-ui-border dark:bg-d-ui-bg dark:hover:border-d-hover-ui-border">
 		<div class="flex flex-row items-center justify-center gap-4">
-			<div class="size-12 overflow-hidden rounded-2xl border-2 bg-blue-400 max-sm:size-14">
+			<div class="size-12 overflow-hidden rounded-2xl bg-white shadow max-sm:size-14">
 				<img src="../assets/projectsIcon/tododev_logo.png" alt="" class=" size-full object-cover">
 			</div>
-			<h1 class=" text-xl font-bold max-sm:text-lg">{{ title }}</h1>
+			<h1 class=" text-xl font-bold text-high-contrast-text dark:text-d-high-contrast-text max-sm:text-lg">{{ title }}</h1>
 		</div>
-		<p class=" text-sm font-medium">{{ description }}</p>
+		<p class=" text-sm font-medium  text-high-contrast-text dark:text-d-high-contrast-text">{{ description }}</p>
 		<div class="flex size-full flex-col items-center justify-center gap-4">
 			<div class="flex flex-wrap justify-center gap-3">
 				<div v-for="tag in tags" :key="tag"
-					class=" rounded-xl border bg-accent-color px-4 py-2 text-xs font-bold shadow-lg dark:bg-d-accent-color/40">{{ tag }}
+					class=" cursor-default rounded-xl border border-ui-border bg-ui-bg px-4 py-2 text-xs font-bold text-low-contrast-text shadow-lg transition-colors duration-75 hover:border-hover-ui-border dark:border-d-ui-border dark:bg-d-ui-bg dark:text-d-low-contrast-text dark:hover:border-d-hover-ui-border">{{ tag }}
 				</div>
 			</div>
 			<a class="group flex flex-row items-center justify-center rounded-md bg-accent-color px-3 py-2 text-base font-bold text-primary transition-all hover:bg-accent-color/70"
