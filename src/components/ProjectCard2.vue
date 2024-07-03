d00d202c6f768b6c636506c0c843bdde9b97af84
DevJ2K <ajavontheopro@gmail.com>
2024-07-03 15:58:03 +0200

    🚧 : New project card design...

---
+++ src/components/ProjectCard2.vue
@@ -0,0 +1,115 @@
+<template>
+	<!-- <a class="select-none" :href="projectLink" target="_blank"> -->
+
+
+		<div class="flex max-w-xs flex-col justify-between overflow-hidden rounded-2xl border border-ui-border bg-ui-bg shadow-lg dark:border-d-ui-border dark:bg-d-ui-bg sm:hidden">
+			<div>
+				<div>
+					<img :src="previewImage" alt="" class="cursor-pointer"  v-on:click="openLink()">
+				</div>
+				<div class="flex flex-col gap-4 p-6">
+					<h1 class=" text-lg font-semibold text-high-contrast-text dark:text-d-high-contrast-text sm:text-xl">{{ title }}</h1>
+
+					<!-- <div class="h-px w-3/4 bg-black/25 dark:bg-white/25"></div> -->
+
+					<p class=" text-sm font-normal  text-high-contrast-text dark:text-d-high-contrast-text">{{ description }}</p>
+
+
+					<div class="flex size-full flex-col items-start justify-start">
+						<div class="flex flex-wrap justify-start gap-2">
+							<div v-for="tag in tags" :key="tag"
+								class=" cursor-default rounded-xl border border-ui-border bg-ui-bg px-4 py-2 text-xs font-semibold text-low-contrast-text shadow-lg transition-colors duration-75 hover:border-hover-ui-border dark:border-d-ui-border dark:bg-d-ui-bg dark:text-d-low-contrast-text dark:hover:border-d-hover-ui-border">{{ tag }}
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div>
+				<div class="h-px w-full bg-ui-border/50 dark:bg-d-ui-border/50"></div>
+				<div class="flex w-full flex-col items-center justify-center gap-2 p-3 md:flex-row">
+
+					<div class="project-card-btn project-card-btn-outline group">
+						<RedirectionSvg color="font-medium text-high-contrast-text group-hover:text-white dark:text-d-high-contrast-text h-4 w-4"/>
+						<h3 class=" text-nowrap text-sm font-medium text-high-contrast-text group-hover:text-white dark:text-d-high-contrast-text">View Project</h3>
+					</div>
+
+					<div class="project-card-btn project-card-btn-fill">
+							<GithubSvg color="text-white dark:text-white h-4 w-4"/>
+							<h3 class="text-sm text-white dark:text-white">GitHub</h3>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	<div class="custom-shadow-lg 768px:hidden flex w-full flex-row overflow-hidden rounded-2xl border-subtle-border bg-subtle-bg dark:border-d-subtle-border dark:bg-d-ui-bg">
+		<div class="w-full">
+			<img :src="previewImage" :alt="title + '_img'" class=" size-full cursor-pointer object-cover" v-on:click="openLink()">
+		</div>
+		<div class="flex flex-col items-start gap-4 p-4">
+			<div class="flex flex-col gap-4">
+
+				<h1 class=" text-lg font-semibold text-high-contrast-text dark:text-d-high-contrast-text sm:text-xl">{{ title }}</h1>
+
+				<p class=" text-sm font-normal  text-high-contrast-text dark:text-d-high-contrast-text">{{ description }}</p>
+
+				<div class="flex size-full flex-col items-start justify-start">
+						<div class="flex flex-wrap justify-start gap-2">
+							<div v-for="tag in tags" :key="tag"
+								class=" custom-shadow-sm cursor-default rounded-xl border-ui-border bg-ui-bg px-4 py-2 text-xs font-semibold text-low-contrast-text transition-colors duration-75 hover:border-hover-ui-border dark:border-d-ui-border dark:bg-d-hover-ui-bg dark:text-d-low-contrast-text dark:hover:border-d-hover-ui-border">{{ tag }}
+							</div>
+						</div>
+				</div>
+			</div>
+
+			<div>
+
+				<div class="flex flex-col items-center justify-center gap-2 pt-2 md:flex-row">
+
+					<div class="project-card-btn-2 project-card-btn-outline group">
+						<GithubSvg color="font-medium text-high-contrast-text dark:text-d-high-contrast-text h-4 w-4"/>
+						<h3 class=" text-nowrap text-sm font-medium text-high-contrast-text dark:text-d-high-contrast-text">Github</h3>
+					</div>
+
+					<div class="project-card-btn-2 project-card-btn-fill">
+							<RedirectionSvg color="text-white dark:text-white h-4 w-4"/>
+							<h3 class="text-sm text-white dark:text-white">View Project</h3>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</template>
+
+<script setup>
+import weatherAppImage from '../assets/projectsPreview/WeatherApp.png';
+import RedirectionSvg from './customSvg/RedirectionSvg.vue';
+import GithubSvg from './logos/GithubSvg.vue';
+
+const props = defineProps({
+	previewImage: {
+		type: String,
+		default: weatherAppImage
+	},
+	title: {
+		type: String,
+		default: "Project Title"
+	},
+	tags: {
+		type: Array,
+		default: () => ["Tag-1", "Tag-2", "Tag-3", "Tag-4", "Tag-5"]
+	},
+	description: {
+		type: String,
+		default: "Nisi adipisicing aliquip reprehenderit ad. Laborum cillum sunt voluptate ad excepteur dolor excepteur officia deserunt qui occaecat qui. Lorem cillum irure quis mollit deserunt."
+	},
+	projectLink: {
+		type: String,
+		default: "http://www.google.com"
+	}
+})
+
+const openLink = () => {
+	window.open(props.projectLink, 'blank');
+}
+</script>
