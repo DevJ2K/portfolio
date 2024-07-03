5d37f4948c978216d3bebc524a79c8005d16178f
DevJ2K <ajavontheopro@gmail.com>
2024-07-03 15:57:16 +0200

    ✅ : Responsive on PresentationView.

--- src/views/home/PresentationView.vue
+++ src/views/home/PresentationView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="relative flex min-h-screen w-full">
     <BackgroundSpotlight color="bg-gray-950" />
-    <main class=" container z-10 flex min-h-screen flex-col items-center justify-center gap-8 sm:gap-12">
+    <main class=" container z-10 flex h-screen min-h-[740px] flex-col items-center justify-center gap-8 sm:gap-12">
         <div class="flex flex-col items-center justify-center gap-2 text-center">
           <h1 class="text-3xl font-extrabold text-high-contrast-text dark:text-d-high-contrast-text sm:text-4xl md:text-5xl">Hello, I'm Theo Ajavon</h1>
           <h2 class="text-2xl font-bold text-accent-color dark:text-d-accent-color sm:text-3xl md:text-4xl">Software Engineer & Swift Developer</h2>
@@ -13,9 +13,9 @@
           <button class="open-resume-btn">Open Resume</button>
 
         <div class="flex w-full flex-row justify-around">
-           <SocialLogo :logo="GithubSvg" name="DevJ2K" size="w-7 h-7"/>
-           <SocialLogo :logo="LinkedinSvg" name="Theo Ajavon" size="w-7 h-7"/>
-           <SocialLogo :logo="EmailSvg" name="ajavontheopro@gmail.com" size="w-7 h-7"/>
+           <SocialLogo :logo="GithubSvg" name="DevJ2K" size="presentation-view-logo"/>
+           <SocialLogo :logo="LinkedinSvg" name="Theo Ajavon" size="presentation-view-logo"/>
+           <SocialLogo :logo="EmailSvg" name="ajavontheopro@gmail.com" size="presentation-view-logo"/>
 
         </div>
     </main>
