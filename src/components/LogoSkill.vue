ad8d3654320b295287d49480a9ce05269174007a
DevJ2K <ajavontheopro@gmail.com>
2024-07-03 15:57:28 +0200

    💭 : Saving status...

--- src/components/LogoSkill.vue
+++ src/components/LogoSkill.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="flex flex-col items-center justify-center gap-2 rounded-lg border border-subtle-border bg-ui-bg p-2 transition-colors hover:border-hover-ui-border dark:border-d-subtle-border dark:bg-d-ui-bg dark:hover:border-d-hover-ui-border">
-		<logo size="w-14 h-10 md:w-16 md:h-10" color=""/>
+		<logo size="w-14 h-8 md:w-16 sm:h-10" color=""/>
 		<h3 class="text-xs font-semibold sm:text-sm">{{ techName }}</h3>
 	</div>
 </template>
