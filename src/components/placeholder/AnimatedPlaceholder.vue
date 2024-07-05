401fa804224418fe49ce0ea98578dd559d693624
DevJ2K <ajavontheopro@gmail.com>
2024-07-05 16:23:42 +0200

    ✅ : Skeletons for projects done !

--- src/components/placeholder/AnimatedPlaceholder.vue
+++ src/components/placeholder/AnimatedPlaceholder.vue
@@ -1,6 +1,6 @@
 <template>
-	<!-- <div class="animate-pulse bg-gradient-to-r from-accent-color/40 dark:from-d-accent-color/40"> -->
-	<div class="animate-pulse bg-gradient-to-r from-red-500 to-black">
+	<div class="animate-pulse bg-gradient-to-r from-accent-color/35 to-accent-color/15 dark:from-d-accent-color/30 dark:to-d-accent-color/15">
+	<!-- <div class="animate-pulse bg-gradient-to-r from-red-500 to-black"> -->
 		&nbsp;
 	</div>
 </template>
--- src/components/placeholder/ProjectCardSkeleton.vue
+++ src/components/placeholder/ProjectCardSkeleton.vue
@@ -4,7 +4,7 @@
 		class="custom-shadow-lg flex max-w-xs flex-col justify-between overflow-hidden rounded-2xl border-ui-border bg-ui-bg dark:border-d-ui-border dark:bg-d-ui-bg  b_project_card:hidden">
 		<div>
 			<div>
-				<AnimatedPlaceholder class=" size-full min-h-44" />
+				<AnimatedPlaceholder class=" size-full h-44" />
 			</div>
 			<div class="flex flex-col gap-4 p-6">
 				<!-- <h1 class=" text-lg font-semibold text-high-contrast-text dark:text-d-high-contrast-text sm:text-xl">Salut</h1> -->
@@ -35,8 +35,60 @@
 			</div>
 		</div>
 	</div>
-	<div class="hidden size-60 bg-red-500 b_project_card:flex">
 
+	<div class="custom-shadow-lg hidden w-full flex-row overflow-hidden rounded-2xl border-subtle-border bg-subtle-bg dark:border-d-subtle-border dark:bg-d-ui-bg b_project_card:flex">
+		<div class="w-9/12">
+			<AnimatedPlaceholder class="size-full"/>
+		</div>
+		<div class="flex w-full flex-col items-start gap-4 p-4">
+			<div class="flex w-full flex-col gap-4">
+
+				<AnimatedPlaceholder class="w-2/6 rounded-full text-lg sm:text-2xl"/>
+
+				<div class="flex w-full flex-col flex-nowrap gap-1">
+					<AnimatedPlaceholder class=" w-full rounded-full text-xs"/>
+					<AnimatedPlaceholder class=" w-10/12 rounded-full text-xs"/>
+					<AnimatedPlaceholder class=" w-11/12 rounded-full text-xs"/>
+				</div>
+				<!-- <p class=" text-sm font-normal  text-high-contrast-text dark:text-d-high-contrast-text">Elit ut ea in ut sint nostrud duis amet ullamco cillum ipsum ut qui dolore. Nisi nulla Lorem et ea magna nisi. Magna irure esse anim magna aute laborum velit consectetur do esse sit Lorem.</p> -->
+
+				<div class="flex size-full flex-col items-start justify-start">
+					<div class="flex flex-wrap justify-start gap-2">
+						<div v-for="_ in 5" :key="_">
+							<AnimatedPlaceholder class="w-16 cursor-default rounded-xl px-4 py-2 text-xs" />
+						</div>
+					</div>
+				</div>
+
+				<!-- <div class="flex size-full flex-col items-start justify-start">
+						<div class="flex flex-wrap justify-start gap-2">
+							<div v-for="tag in tags" :key="tag"
+								class=" custom-shadow-sm cursor-default rounded-xl border-ui-border bg-ui-bg px-4 py-2 text-xs font-semibold text-low-contrast-text transition-colors duration-75 hover:border-hover-ui-border dark:border-d-ui-border dark:bg-d-hover-ui-bg dark:text-d-low-contrast-text dark:hover:border-d-hover-ui-border">{{ tag }}
+							</div>
+						</div>
+				</div> -->
+			</div>
+
+			<div class="">
+
+				<div class="flex flex-col items-center justify-center gap-2 pt-2 md:flex-row">
+
+					<AnimatedPlaceholder class="h-full w-24 rounded-2xl px-3 py-2 text-sm"/>
+
+					<!-- <div class="project-card-btn project-card-btn-outline group h-full">
+						<GithubSvg color="font-medium text-high-contrast-text dark:text-d-high-contrast-text h-4 w-4"/>
+						<h3 class=" text-nowrap text-sm font-medium text-high-contrast-text dark:text-d-high-contrast-text">Github</h3>
+					</div> -->
+
+					<AnimatedPlaceholder class="h-full w-36 rounded-2xl px-3 py-2 text-sm"/>
+					<!-- <div class="project-card-btn project-card-btn-fill h-full text-nowrap">
+							<RedirectionSvg color="text-white dark:text-white h-4 w-4"/>
+							<h3 class="text-sm text-white dark:text-white">View Project</h3>
+					</div> -->
+				</div>
+			</div>
+
+		</div>
 	</div>
 
 	<!-- <div class="flex w-full rounded-md bg-red-500 px-3 py-6 shadow-md">
