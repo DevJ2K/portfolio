d94502a502d0e7e0bf1ab0d131ae6627258ad88a
DevJ2K <ajavontheopro@gmail.com>
2024-07-05 12:27:53 +0200

    💭 : Rename home files...

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,11 +18,11 @@
 
 <script setup>
 import NavigationBar from '@/components/NavigationBar.vue';
-import PresentationView from './home/PresentationView.vue'
+import PresentationView from './home/PresentationTab.vue'
 import AboutView from './home/AboutView.vue'
-import SkillView from './home/SkillView.vue';
-import ProjectsView from './home/ProjectsView.vue'
-import ContactView from './home/ContactView.vue';
+import SkillView from './home/SkillTab.vue';
+import ProjectsView from './home/ProjectsTab.vue'
+import ContactView from './home/ContactTab.vue';
 import CustomFooter from './home/CustomFooter.vue';
 import BgStars from '@/components/background/BgStars.vue';
 </script>
