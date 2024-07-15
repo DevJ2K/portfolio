54356af71e861aeacecfd623e88b2b6e49b61bfd
DevJ2K <ajavontheopro@gmail.com>
2024-07-15 17:07:10 +0200

    💭 : Try to find performance < 60 and is probably link...

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import viteCompression from 'vite-plugin-compression';
 import localdata from './local-data.json'
 import aboutdata from './about-data.json'
 
@@ -9,6 +10,18 @@ import aboutdata from './about-data.json'
 export default defineConfig({
   plugins: [
     vue(),
+    viteCompression({
+      // Options pour gzip
+      algorithm: 'gzip',
+      ext: '.gz',
+      deleteOriginFile: false, // Ne pas supprimer les fichiers originaux
+    }),
+    viteCompression({
+      // Options pour Brotli
+      algorithm: 'brotliCompress',
+      ext: '.br',
+      deleteOriginFile: false, // Ne pas supprimer les fichiers originaux
+    })
   ],
   define: {
     'localData': localdata,
