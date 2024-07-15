1131d75a3f30a42ab1785b1dc6761417c39cdc7e
DevJ2K <ajavontheopro@gmail.com>
2024-07-15 13:57:40 +0200

    💭 : Optimizing App...

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,7 +7,6 @@
     <AboutView id="about"/>
     <ContactView id="contact"/>
     <CustomFooter/>
-    <BgStars/>
   </main>
   <!-- <div>
     <h1 class=" bg-blue-400">Portfolio</h1>
@@ -22,7 +21,6 @@ import SkillView from './home/SkillTab.vue';
 import ProjectsView from './home/ProjectsTab.vue'
 import ContactView from './home/ContactTab.vue';
 import CustomFooter from './home/CustomFooter.vue';
-import BgStars from '@/components/background/BgStars.vue';
 import refreshBackground from '@/js/refreshBackground';
 
 import { onMounted } from 'vue';
