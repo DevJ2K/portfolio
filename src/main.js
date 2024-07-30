86af9cebdbdf836287a8ab669a29e5d234a252f5
theo <ajavontheopro@gmail.com>
2024-07-30 06:04:34 +0200

    💭 : Change size value and move await top-level

--- src/main.js
+++ src/main.js
@@ -7,15 +7,18 @@ import router from './router'
 import canvasSize from 'canvas-size';
 
 
-
-const { success, width, height } = await canvasSize.maxArea();
-// console.log(":" + success);
-if (success) {
-	// console.log(width);
-	// console.log(height);
-	document.getElementById("__maxWidth").innerHTML = width;
-	document.getElementById("__maxHeight").innerHTML = height;
+const initCanvasSize = async () => {
+	const { success, width, height } = await canvasSize.maxArea();
+	// console.log(":" + success);
+	if (success) {
+		// console.log(width);
+		// console.log(height);
+		document.getElementById("__maxWidth").innerHTML = width;
+		document.getElementById("__maxHeight").innerHTML = height;
+	}
 }
+
+initCanvasSize();
 
 const app = createApp(App)
 
