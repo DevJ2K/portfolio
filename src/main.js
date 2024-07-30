3eb42df4963b89ffdcc7d2ab5c45ee15d2de6057
theo <ajavontheopro@gmail.com>
2024-07-30 05:33:25 +0200

    🚧 : Editing particlesJs and trying to fix mobile background...

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import canvasSize from 'canvas-size';
+
+
+const { success, width, height } = await canvasSize.maxArea();
+console.log(success);
+// log(results.success ? '🟢' : '🔴', results);
+
 const app = createApp(App)
 
 app.use(router)
