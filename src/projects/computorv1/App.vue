709413e017c5f3bd6705bbc83c8572b52f5c1c28
DevJ2K <ajavontheopro@gmail.com>
2024-09-06 10:49:59 +0200

    🚧 : Fix computorv1 scroll background color...

--- src/projects/computorv1/App.vue
+++ src/projects/computorv1/App.vue
@@ -1,6 +1,6 @@
 <template>
 	<NavigationBar/>
-	<main class="dark relative z-10 flex size-full min-h-screen flex-col items-center bg-computorv1-app-bg font-Roboto">
+	<main class="dark relative z-10 flex size-full min-h-screen w-screen flex-col items-center bg-computorv1-app-bg font-Roboto">
 		<div class="h-24"></div>
 		<HomeView v-if="$route.name === 'computorv1'"/>
 		<LearnMathView v-if="$route.name === 'computorv1/learn-math'"/>
@@ -10,9 +10,26 @@
 
 <script setup>
 
+import { onMounted, onUnmounted } from 'vue';
 import NavigationBar from './components/navbar/NavigationBar.vue';
 import HomeView from './views/HomeView.vue';
 import LearnMathView from './views/LearnMathView.vue';
 import TestView from './views/TestView.vue';
 
+onMounted(() => {
+	console.log("Monter")
+	const mainApp = document.getElementById("mainApp");
+	console.log(mainApp)
+	mainApp.classList.remove("bg-app-bg");
+	mainApp.classList.remove("bg-d-app-bg");
+	mainApp.classList.add("bg-computorv1-app-bg");
+})
+
+onUnmounted(() => {
+	const mainApp = document.getElementById("mainApp");
+	mainApp.classList.add("bg-app-bg");
+	mainApp.classList.add("bg-d-app-bg");
+	mainApp.classList.remove("bg-computorv1-app-bg");
+	// console.log("Demonter")
+})
 </script>
