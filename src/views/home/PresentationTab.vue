9a00961dbbdb7eada537cb43330df00319fe12c7
theo <ajavontheopro@gmail.com>
2025-05-01 01:13:28 +0200

    💭 : Edit some stuff...

--- src/views/home/PresentationTab.vue
+++ src/views/home/PresentationTab.vue
@@ -6,15 +6,14 @@
 
         <div class="h-1/6 w-full max-sm:hidden"></div>
         <div class="flex flex-col items-center justify-center gap-2 text-center">
-          <h1 class="text-3xl font-extrabold text-high-contrast-text dark:text-d-high-contrast-text sm:text-4xl md:text-5xl" data-aos="zoom-in" data-aos-duration="1200">Hello, I'm Theo Ajavon</h1>
+          <h1 class="text-3xl font-extrabold text-high-contrast-text dark:text-d-high-contrast-text sm:text-4xl md:text-5xl" data-aos="zoom-in" data-aos-duration="1200">Hey, I’m Theo — aka DevJ2K</h1>
           <h2 class="text-2xl font-bold text-accent-color dark:text-d-accent-color sm:text-3xl md:text-4xl" data-aos="zoom-in" data-aos-delay="100" data-aos-duration="1200">Software Engineer & Student</h2>
         </div>
         <p class="text-center text-base font-normal text-high-contrast-text dark:text-d-high-contrast-text sm:text-lg md:text-xl" data-aos="zoom-in"  data-aos-delay="200" data-aos-duration="1200">Based in Paris and studying at <a href="https://42.fr/en/homepage/" target="_blank" class=" cursor-pointer underline decoration-accent-color underline-offset-4 dark:decoration-d-accent-color">42</a>, I’m a 19 years old passionate about exploring cutting-edge technologies and mastering new skills. My goal is to drive impactful projects that challenge the mind and push boundaries.</p>
 
-          <div  data-aos="zoom-in" data-aos-delay="300" data-aos-duration="1200">
-            <!-- <button class="open-resume-btn" @click="openCV">Open Resume</button> -->
+          <!-- <div data-aos="zoom-in" data-aos-delay="300" data-aos-duration="1200">
             <a class="open-resume-btn" href="CV.pdf" target="_blank">Open Resume</a>
-          </div>
+          </div> -->
 
         <div class="flex w-full flex-row items-center justify-around sm:h-1/6" data-aos="zoom-in" data-aos-delay="400" data-aos-offset="0" data-aos-duration="1200">
            <SocialLogo :logo="GithubSvg" name="DevJ2K" size="presentation-view-logo" link="https://github.com/DevJ2K"/>
